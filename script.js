adcc0d57ddc7fc591cf939d0c815dd478419ce9c
git status <215340863+g-sts@users.noreply.github.com>
2025-06-22 16:08:06 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 const button = document.querySelector('#btn-add-task')
 const input = document.querySelector('#input-task')
+const listaCompleta = document.querySelector('#list-task')
 
 let minhaListaDeItens = []
 
 function adicionarNovaTarefa(){
     minhaListaDeItens.push(input.value)
+
+
+    input.value = ''
 
     listarTarefas()
 }
@@ -12,18 +16,31 @@ function adicionarNovaTarefa(){
 function listarTarefas(){
     let novaLi = ''
 
-    minhaListaDeItens.forEach( tarefa =>{
+    minhaListaDeItens.forEach( (tarefa, index) =>{
 
         novaLi = novaLi + ` 
 
             <li class="task">
-                <img src="img/checked.png" alt="check-image">
+                <img src="img/checked.png" alt="check-image" onclick = "tarefaConcluida(${index})">
                 <p>${tarefa}</p>
-                <img src="img/trash.png" alt="trash-image">
+                <img src="img/trash.png" alt="trash-image" onclick = "deletarItem(${index})">
             </li>
 
             `
     } )
+    listaCompleta.innerHTML = novaLi
+}
+
+function tarefaConcluida(index){
+    
+
+
+}
+
+function deletarItem(index){
+    minhaListaDeItens.splice(index, 1)
+
+    listarTarefas()
 }
 
 button.addEventListener('click', adicionarNovaTarefa )
